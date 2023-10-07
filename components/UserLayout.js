a84a47958d3b6f2efb9d2742043164f3b8f906e6
Saleem Mohammed <salmoham@u90z02s03.42kl.edu.my>
2023-10-07 22:05:48 +0800

    fix: build error

--- components/UserLayout.js
+++ components/UserLayout.js
@@ -74,7 +74,7 @@ function Header() {
             <header>
                 <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="/">42 Event</a>
+                        <Link className="navbar-brand" href="/">42 Event</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
